10bfe137c8b4d734dca9bf87398a68a76c478d45
raluca_adam-admin <raluca@adamii.ro>
2025-11-05 21:38:48 +0200

    created instruction factory

---
+++ Simulator-Backend/src/model/instruction/InstructionFactory.java
@@ -0,0 +1,28 @@
+package model.instruction;
+
+/**
+ * Instruction Factory
+ * Parses 32-bit binary instructions and creates appropriate objects
+ */
+public class InstructionFactory {
+    
+    /**
+     * Parse a 32-bit binary instruction word into an Instruction object
+     * 
+     * @param binaryWord 32-bit instruction word
+     * @return Instruction object
+     */
+    public static Instruction parseInstruction(int binaryWord) {
+
+        int opcode = (binaryWord >> 26) & 0x3F;
+        
+        if (opcode == 0x00) {
+            return new RTypeInstruction(opcode, binaryWord);
+        } else if (opcode == 0x02 || opcode == 0x03) {
+            return new JTypeInstruction(opcode, binaryWord);
+        } else {
+            return new ITypeInstruction(opcode, binaryWord);
+        } 
+    }
+}
+
